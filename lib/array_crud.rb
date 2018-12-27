b2a020b262dbefd659ea2a46e62cb3cd6a1f1ba0
A Glazer <avigayilglazer@gmail.com>
2018-12-27 20:05:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   expression = ["hi", "bye", "whatever", "shortly"]
-  expression << "arrays"
+  expression << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
